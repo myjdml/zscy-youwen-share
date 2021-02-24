f95adb4feda5c60e75c4997614791a699c36ebbf
myjdml <898762316@qq.com>
2021-02-24 09:29:26 +0800

    update: token

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,14 @@
  * @Author: myjdml
  * @Date: 2021-02-09 00:20:19
  * @LastEditors: myjdml
- * @LastEditTime: 2021-02-24 00:21:12
+ * @LastEditTime: 2021-02-24 09:29:14
  * @FilePath: /zscy-youwen-share/src/server/index.js
  * @Description: nothing is everything
  */
 import axios from 'axios'
 
 // const TOKEN = 'Bearer ' + `${localStorage.getItem('id_token')}`
-const TOKEN = 'Bearer ' + 'eyJjb2xsZWdlIjoi6L2v5Lu25bel56iL5a2m6ZmiIiwiZXhwIjoiMTYxNDE0MDQyMiIsImdlbmRlciI6IueUtyIsImhlYWRJbWdVcmwiOiJodHRwOi8vY2RuLnJlZHJvY2sudGVhbS9tYWdpcG9rZV8yMDE5MjEzODk1XzE2MDk0MDc3MTEucG5nIiwiaWF0IjoiMTYxNDA5NzIyMiIsImludHJvZHVjdGlvbiI6IiIsIm5pY2tuYW1lIjoiY29kZXIiLCJwaG9uZSI6IiIsInFxIjoiIiwicmVhbE5hbWUiOiLmr5vlsKfkv4oiLCJyZWRpZCI6IjQwMTU2NDkzZmQxM2VlOGEyYjQyMzFjY2VjMzk2ODRiN2IyMDllZGMiLCJzdHVOdW0iOiIyMDE5MjEzODk1Iiwic3ViIjoicG9ja2V0In0=.j7e8C8DOc0kC0C2yKBhsv1Cxa/Bbi5CKPsUowza43T47+TZg9LhQ5TznDwZdMiYsy88Kuc4NL78MDWMolt1Yd3KwRySrW+WoPsDcaSCibZILmRZsGfLUiYpz42NTQtR3UfZ1yWdDeoWupIw2nMMDNlb0x47mWg7Ean0ynHqAPs6rKhyjZUTFcaulzrd4s38wgAVO8D04cylPJnAHMLo3StE4TBqjOfWqaKdTpuKBTtLsQC5QfYZJYdSTfZzj/x3aaHwMlXDu5rflPxkPHQ7kEVx6jQWWpRfnCh+K0M7+tdCWbepwLfVchq/i+X9MPHFgjQ8JWrZKwsSyIFb3pz9Fpw=='
+const TOKEN = 'Bearer ' + 'eyJjb2xsZWdlIjoi6L2v5Lu25bel56iL5a2m6ZmiIiwiZXhwIjoiMTYxNDE3MzI4OCIsImdlbmRlciI6IueUtyIsImhlYWRJbWdVcmwiOiJodHRwOi8vY2RuLnJlZHJvY2sudGVhbS9tYWdpcG9rZV8yMDE5MjEzODk1XzE2MDk0MDc3MTEucG5nIiwiaWF0IjoiMTYxNDEzMDA4OCIsImludHJvZHVjdGlvbiI6IiIsIm5pY2tuYW1lIjoiY29kZXIiLCJwaG9uZSI6IiIsInFxIjoiIiwicmVhbE5hbWUiOiLmr5vlsKfkv4oiLCJyZWRpZCI6IjQwMTU2NDkzZmQxM2VlOGEyYjQyMzFjY2VjMzk2ODRiN2IyMDllZGMiLCJzdHVOdW0iOiIyMDE5MjEzODk1Iiwic3ViIjoicG9ja2V0In0=.G2Yj+H1JlUBfdWkxZ+SiQseljbhwNZYFWd4fB1OQ9OYFJ8k9iXuHYkBf9ynbW/g95Tt15tpOZSKMjGII3PGzKXAI5+6nVywqAuPvsdLyIPAupJzkw2drl6DqebLwzhUEqMyQqZ59hWO8xxXXlQYQshb87mG/skwl+a7JX0wxHvXcHSu1OIqc35MYmkl1CemqNsg8me2ldTrhWmsOqc/OuXiPUS9w1LHoX/iEsMVMAJMA82/HMVRCjAFq61s8E3/Jx89yg0kq1o/1VYY0db5SHWczxn0Sta/fRJcZ8CFbku1iMw/WLDHdUUrEuOmdbyHDmtBmAj9vvH9N/0c1+UKJLg=='
 const BACEURL = process.env.NODE_ENV === 'production' ? 'https://cyxbsmobile.redrock.team/wxapi/magipoke-loop' : '/api'
 axios.defaults.headers.common.Authorization = TOKEN
 
