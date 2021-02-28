5e4d31efcc5ad3eee6acd8d08ef6aea57818a695
myjdml <898762316@qq.com>
2021-02-28 16:07:50 +0800

    fix: change ajax

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
  * @Author: myjdml
  * @Date: 2021-02-09 00:20:19
  * @LastEditors: myjdml
- * @LastEditTime: 2021-02-24 13:06:22
+ * @LastEditTime: 2021-02-28 16:07:31
  * @FilePath: /zscy-youwen-share/src/server/index.js
  * @Description: nothing is everything
  */
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 // const TOKEN = 'Bearer ' + `${localStorage.getItem('id_token')}`
 const TOKEN = 'Bearer ' + 'eyJjb2xsZWdlIjoi6L2v5Lu25bel56iL5a2m6ZmiIiwiZXhwIjoiMTYxNDE4NjMyNiIsImdlbmRlciI6IueUtyIsImhlYWRJbWdVcmwiOiJodHRwOi8vY2RuLnJlZHJvY2sudGVhbS9tYWdpcG9rZV8yMDE5MjEzODk1XzE2MDk0MDc3MTEucG5nIiwiaWF0IjoiMTYxNDE0MzEyNiIsImludHJvZHVjdGlvbiI6IiIsIm5pY2tuYW1lIjoiY29kZXIiLCJwaG9uZSI6IiIsInFxIjoiIiwicmVhbE5hbWUiOiLmr5vlsKfkv4oiLCJyZWRpZCI6IjQwMTU2NDkzZmQxM2VlOGEyYjQyMzFjY2VjMzk2ODRiN2IyMDllZGMiLCJzdHVOdW0iOiIyMDE5MjEzODk1Iiwic3ViIjoicG9ja2V0In0=.cZbiI1f/KIInFBhCgcmaAE6/pP+Tu7GNwCyIZszYx1hbTHnt+SP289Hp2LXdDIGGFwHr0Gtsn1az4HhWIN0hH6qBlV70lgVP2j1k7WH4EJZKfsx4gSwTX4vyCJq861U9w2xMskqncuIXdCuTYHfT4cnNyFXYRq8Ipyalm+wM3Oforq511ut4H85uW+toOwdt5d0/BrmCFRKfpGYm36A2J6q2QAIkjT4mJz/Hm0Lus1qIaJlL3RFpjR23PKIu6qol4Oq77HIVsvnBAjn4HHAArGvKtzexor0I3YnXd2d+qcrIE2x8FYnQ8E9DNcKueEwxJk3gpIeD9kcht/LZjqoyDA=='
-const BACEURL = process.env.NODE_ENV === 'production' ? 'https://cyxbsmobile.redrock.team/wxapi/magipoke-loop' : '/api'
+const BACEURL = process.env.NODE_ENV === 'production' ? 'https://be-prod.redrock.team/wxapi/magipoke-loop' : '/api'
 axios.defaults.headers.common.Authorization = TOKEN
 
 export const instance = axios.create({
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@
  * @Author: myjdml
  * @Date: 2021-02-07 18:44:52
  * @LastEditors: myjdml
- * @LastEditTime: 2021-02-28 12:37:40
+ * @LastEditTime: 2021-02-28 13:52:56
  * @FilePath: /zscy-youwen-share/vue.config.js
  * @Description: nothing is everything
  */
 module.exports = {
   outputDir: 'build',
-  publicPath: process.env.NODE_ENV === 'production' ? '/welcome-2020/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/zscy-youwen-share/' : './',
   chainWebpack: config => {
     const oneOfsMap = config.module.rule('scss').oneOfs.store
     oneOfsMap.forEach(item => {
