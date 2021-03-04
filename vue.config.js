746df3a0c2885f68e95e1e08e19b2fb440cff22d
myjdml <898762316@qq.com>
2021-03-04 18:01:38 +0800

    fix: change vue.config

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@
  * @Author: myjdml
  * @Date: 2021-02-07 18:44:52
  * @LastEditors: myjdml
- * @LastEditTime: 2021-02-28 13:52:56
+ * @LastEditTime: 2021-03-04 17:57:42
  * @FilePath: /zscy-youwen-share/vue.config.js
  * @Description: nothing is everything
  */
 module.exports = {
   outputDir: 'build',
-  publicPath: process.env.NODE_ENV === 'production' ? '/zscy-youwen-share/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/game/zscy-youwen-share/' : './',
   chainWebpack: config => {
     const oneOfsMap = config.module.rule('scss').oneOfs.store
     oneOfsMap.forEach(item => {
