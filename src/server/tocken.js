a64a2ba5fc29d2c9d05d341821b067203e343687
myjdml <898762316@qq.com>
2021-03-07 14:12:59 +0800

    fix: ci build

--- src/server/tocken.js
+++ src/server/tocken.js
@@ -2,16 +2,16 @@
  * @Author: myjdml
  * @Date: 2021-02-19 14:24:20
  * @LastEditors: myjdml
- * @LastEditTime: 2021-02-19 14:25:20
+ * @LastEditTime: 2021-03-07 14:12:36
  * @FilePath: /zscy-youwen-share/src/server/tocken.js
  * @Description: nothing is everything
  */
-import axios from 'axios'
+// import axios from 'axios'
 
-export const instance = axios.create({
-  baseURL: BACEURL,
-  timeout: 50000,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-})
+// export const instance = axios.create({
+//   baseURL: BACEURL,
+//   timeout: 50000,
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded'
+//   }
+// })
