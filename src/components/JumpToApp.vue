43a5c0d5affa796c83173ed8e934d11a19b3ef4b
myjdml <898762316@qq.com>
2021-03-07 14:32:15 +0800

    add: jump to app

--- src/components/JumpToApp.vue
+++ src/components/JumpToApp.vue
@@ -2,7 +2,7 @@
  * @Author: myjdml
  * @Date: 2021-02-07 18:44:52
  * @LastEditors: myjdml
- * @LastEditTime: 2021-03-07 12:18:22
+ * @LastEditTime: 2021-03-07 14:26:53
  * @FilePath: /zscy-youwen-share/src/components/JumpToApp.vue
  * @Description: nothing is everything
 -->
@@ -10,20 +10,61 @@
   <div id="jump-to-app">
     <div class="cover"></div>
 
-    <div class="btn">App内打开</div>
+    <div class="btn" @click="jump" >App内打开</div>
   </div>
 </template>
 
 <script>
-// import parseQueryString from '../utils/parseQueryString'
+import parseQueryString from '../utils/parseQueryString'
+import CallApp from 'callapp-lib'
 
 export default {
   name: 'JnmpToApp',
   data () {
     return {
+      zyOptions: parseQueryString(window.location.href)
     }
   },
   methods: {
+    jump () {
+      const options = {
+        scheme: {
+          protocol: 'cyxbs',
+          host: 'redrock.team'
+        },
+        intent: {
+          package: 'com.mredrock.cyxbs',
+          scheme: 'cyxbs',
+          host: 'redrock.team'
+        },
+        // universal: {
+        //   host: 'oia.zhihu.com/question/270839820/answer/477722658',
+        //   pathKey: 'action',
+        // },
+        appstore: 'https://itunes.apple.com/cn/app/id974026615',
+        // yingyongbao: '//a.app.qq.com/o/simple.jsp?pkgname=com.zhihu.android',
+        fallback: 'https://wx.redrock.team/app/#/',
+        timeout: 2000
+      }
+      const callLib = new CallApp(options)
+
+      callLib.open({
+        param: {
+          id: this.zyOptions.id
+        },
+        path: 'dynamic_detail/qa/entry'
+      })
+    },
+    judgeMobileType () {
+      var u = navigator.userAgent
+      if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
+        return 'Android'
+      } else if (u.indexOf('iPhone') > -1) {
+        return 'IOS'
+      } else if (u.indexOf('Windows Phone') > -1) {
+        return 'WinPhone'
+      }
+    }
   },
   mounted () {
     // const zyOptions = parseQueryString(window.location.href)
